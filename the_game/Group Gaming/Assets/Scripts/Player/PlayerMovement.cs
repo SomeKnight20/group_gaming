fb247d72eb56c401fe770cb6ed368157954a4a9d
SomeKnight20 <teemu.huttunen20@gmail.com>
2023-08-09 17:38:20 +0300

    crawling ja ledge grab

--- the_game/Group Gaming/Assets/Scripts/Player/PlayerMovement.cs
+++ the_game/Group Gaming/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,12 +8,21 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D player;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform ceilingCheck;
+
+
+    public GameObject grabber;
+    public Transform wallCheck;
+    public Transform aboveWallCheck;
+    private bool isGrabbingLedge = false;
+
 
     float horizontalInput;
     float verticalInput;
 
     private Vector2 movement;
-    public float moveSpeed = 5f;
+    public float baseMoveSpeed = 5f;
+    private float moveSpeed = 0f;
     public float jumpForce = 5f;
 
     public bool isFlying = false;
@@ -22,25 +31,61 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        grabber.SetActive(false);
+        moveSpeed = baseMoveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown("space") && IsGrounded()){
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //jumping
+        if (Input.GetKeyDown("space") && IsGrounded() || Input.GetKeyDown("space") && isGrabbingLedge){
             player.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
         if (Input.GetKeyUp("space") && player.velocity.y > 0f){
             player.velocity = new Vector2(movement.x, player.velocity.y * 0.5f);
         }
-        
+
+        if (Input.GetKey("left shift")){
+            moveSpeed = baseMoveSpeed/2;
+            animator.speed = 0.5f;
+        }else{
+            moveSpeed = baseMoveSpeed;
+            animator.speed = 1;
+        }
+
+        //crawling
+        if (verticalInput < 0  && IsGrounded() || CeilingCheck() && IsGrounded()){
+            moveSpeed = baseMoveSpeed/2;
+            animator.speed = 0;
+            animator.SetBool("IsCrawling", true);
+            if(horizontalInput != 0){
+                animator.speed = 0.5f;
+            }
+        }else{
+            animator.SetBool("IsCrawling", false);
+            // animator.speed = 1;
+        }
+
+        //ledge grabbing
+        if(WallCheck() && !AboveWallCheck() && verticalInput >= 0){
+            grabber.SetActive(true);
+            animator.SetBool("IsGrappingLedge", true);
+            isGrabbingLedge = true;
+        }else{
+            grabber.SetActive(false);
+            isGrabbingLedge = false;
+            animator.SetBool("IsGrappingLedge", false);
+        }
         
 
-
-        // movement.x = horizontalInput;
+    
         movement.x = horizontalInput * moveSpeed;
 
+        //flip player
         if (horizontalInput > 0){
             transform.localScale = new Vector3(-1, 1, 1);
         }
@@ -51,7 +96,6 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
         animator.SetFloat("FallingSpeed", player.velocity.y);
         animator.SetBool("IsAttacking", Input.GetKey("e"));
-        animator.SetBool("IsSitting", Input.GetKey("left shift"));
     }
 
     void FixedUpdate()
@@ -67,4 +111,25 @@ public class PlayerMovement : MonoBehaviour
         }
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
+
+    private bool CeilingCheck()
+    {
+        return Physics2D.OverlapCircle(ceilingCheck.position, 0.2f, groundLayer);
+    }
+
+    private bool WallCheck()
+    {
+        return Physics2D.OverlapBox(wallCheck.position, new Vector2(0.1f, 0.2f), 0, groundLayer);
+    }
+    private bool AboveWallCheck()
+    {
+        return Physics2D.OverlapBox(aboveWallCheck.position, new Vector2(0.2f, 0.1f), 0, groundLayer);
+    }
+    // void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     // if (collision.gameObject.CompareTag("WallCheck"))
+    //     // {
+    //         Debug.Log("WallCheck");
+    //     // }
+    // }
 }
